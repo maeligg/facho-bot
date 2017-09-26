0c4e220b92e1ed85f798e1bad1516c38a6216da9
Maël Brunet <mbt@emakina.com>
2017-09-26 14:52:49 +0200

    Ajout de nouveaux mots

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const Twit = require('twit');
 // Uncomment these lines if running locally (see readme for more details)
-// const config = require('./config.js');
-// process.env = config;
+const config = require('./config.js');
+process.env = config;
 
 
 // Words arrays
-const noms = ['des journalopes', 'des journaputes', 'des socialopes', 'des gauchiasses', 'des voyoucrates', 'des merdias', 'des feminazis', 'des islamo-gauchistes', 'des illuminatis', 'des sionistes', 'des lobbies'];
+const noms = ['journalopes', 'journaputes', 'socialopes', 'gauchiasses', 'voyoucrates', 'merdias', 'feminazis', 'islamo-gauchistes', 'illuminatis', 'sionistes', 'lobbies', 'franc-maçons', 'euro-atlantistes', 'yankees', 'idiots utiles', 'oligarques', 'élites', 'propagandistes', 'antiracistes', 'bureaucrates', 'banksters', 'satanistes'];
 
-const adjectifs = ['cosmopolites', 'bobos', 'droit de l\'hommistes', 'bien pensants', 'politiquement corrects'];
+const adjectifs = ['cosmopolites', 'bobos', 'droit de l\'hommistes', 'bien pensants', 'politiquement corrects', 'libre-échangistes', 'décadents', 'capitalistes', 'déracinés', 'mondialistes', 'communautaristes', 'impérialistes', 'immigrationnistes', 'droitards'];
 
-const phraseDébut = ['Encore un coup', 'Encore un example', 'Triste example', 'C\'est la faute', 'Il serait temps de se débarasser', 'Vous êtes à la solde', 'Vous faites le jeu'];
+const phraseDébut = ['Encore un coup', 'Encore un example', 'Triste example', 'C\'est la faute', 'Il serait temps de se débarasser', 'Vous êtes à la solde', 'Vous faites le jeu', 'Tout ça à cause', 'Toujours pas débarrassés', 'Marre', 'On n\'en peut plus', 'Ras-le-bol', 'Ras la casquette', 'Y\'en a assez'];
 
-const phraseFin = ['dirigent ce pays', 'contrôlent les médias', 'contrôlent la presse', 'contrôlent le système', 's\'autorisent tout'];
+const phraseFin = ['dirigent ce pays', 'contrôlent les médias', 'contrôlent la presse', 'contrôlent le système', 's\'autorisent tout', 'détruisent la tradition française', 'perpétuent la théorie du genre', 'anéantissent l\'identité française', 'veulent gouverner par le chaos', 'diabolisent les identitaires', 'veulent mettre en place un gouvernement mondial', 'tirent les ficelles', 'sont les mains occultes'];
 
-const interjectionDébut = ['Une honte !', 'Honteux !', 'Honte à vous !', 'Dégoûtant.', 'N\'importe quoi.', 'Scandale !', 'Gerbant.', 'Affligeant.'];
+const interjectionDébut = ['Une honte !', 'Honteux !', 'Honte à vous !', 'Dégoûtant.', 'N\'importe quoi.', 'Scandale !', 'Gerbant.', 'Affligeant.', 'C\'est petit !', 'Média-mensonge !', 'Quelle bouillie !', 'Article erroné !', 'Article bâclé !', 'Faux !', 'Vous cachez la vérité !', 'La vérité est ailleurs !', 'Mensonge !', 'Menteurs !', 'Traîtres !'];
 
-const interjectionFin = ['Jeanne, au secours !', 'Pétain n\'aurait pas cautionné.', 'Pétain doit se retourner dans sa tombe.', 'J\'ai honte d\'être Français.', 'Virons-les !', 'Demandez l\'avis de Faurisson pour voir.', 'Retournez manger un couscous avec Philippot.', 'Ça mérite une bonne quenelle.', 'Allez lire le dernier bouquin de Soral.', 'Allez réécouter la dernière intervention de Zemmour.', 'Il faut quitter l\'Union Européenne.', 'L\'Islam va trop loin.', 'Nos valeurs se perdent.', '#LePen2021', '#Marion2021'];
+const interjectionFin = ['Jeanne, au secours !', 'Pétain n\'aurait pas cautionné.', 'Pétain doit se retourner dans sa tombe.', 'J\'ai honte d\'être Français.', 'Virons-les !', 'Demandez l\'avis de Faurisson pour voir.', 'Retournez manger un couscous avec Philippot.', 'Ça mérite une bonne quenelle.', 'Allez lire le dernier bouquin de Soral.', 'Allez réécouter la dernière intervention de Zemmour.', 'Nos valeurs se perdent.', '#LePen2021', '#Marion2021', 'On est loin de la pudeur hélléno-chrétienne.', 'Rejoignez plutôt la vraie dissidence !', 'Encore de la novlangue journalistique !', 'Vous déshonnorez la liberté de la presse.', 'Où est passée la liberté d\'expression?', 'Le génie français se perd...', 'La révolte des peuples va arriver.', 'Il y a des coups de savate qui se perdent.', 'Où est l\'authentique journalisme?', 'Fini, l\'âge d\'or de notre civilisation...'];
 
 
 // Utils
@@ -37,7 +37,7 @@ const tweet = () => {
   const tweetContent =
       (coinFlip() === 0 ? randomItem(interjectionDébut) + ' ' : '')
       + randomItem(phraseDébut)
-      + ' '
+      + ' des '
       + randomItem(noms)
       + (coinFlip() === 0 ?
       ' qui '
