700a36a3097a4bf58bc2d372a28be353101685ab
Maël Brunet <mbt@emakina.com>
2017-09-26 10:47:20 +0200

    ajout de la logique pour répondre aux tweets de la liste

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./config.js');
 
 process.env = config;
 
+
 // Words arrays
 const noms = ['des journalopes', 'des journaputes', 'des socialopes', 'des gauchiasses', 'des voyoucrates', 'des merdias', 'des feminazis', 'des islamo-gauchistes', 'des illuminatis', 'des sionistes', 'des lobbies'];
 
@@ -16,49 +17,50 @@ const interjectionDébut = ['Une honte !', 'Honteux !', 'Honte à vous !', 'Dég
 
 const interjectionFin = ['Jeanne, au secours !', 'Pétain n\'aurait pas cautionné.', 'Pétain doit se retourner dans sa tombe.', 'J\'ai honte d\'être Français.', 'Virons-les !', 'Demandez l\'avis de Faurisson pour voir.', 'Retournez manger un couscous avec Philippot.', 'Ça mérite une bonne quenelle.', 'Allez lire le dernier bouquin de Soral.', 'Allez réécouter la dernière intervention de Zemmour.', 'Il faut quitter l\'Union Européenne.', 'L\'Islam va trop loin.', 'Nos valeurs se perdent.', '#LePen2021', '#Marion2021'];
 
+
 // Utils
 const randomItem = array => array[Math.floor(Math.random() * array.length)];
 
 const coinFlip = () => Math.floor(Math.random() * 2);
 
 const T = new Twit({
-    consumer_key: process.env.consumer_key,
-    consumer_secret: process.env.consumer_secret,
-    access_token: process.env.access_token,
-    access_token_secret: process.env.access_token_secret,
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token: process.env.access_token,
+  access_token_secret: process.env.access_token_secret,
 });
 
 
 // Build and post the tweet
 const tweet = () => {
-    // Build the tweet
-    const tweetContent =
-        (coinFlip() === 0 ? randomItem(interjectionDébut) + ' ' : '')
-        + randomItem(phraseDébut)
-        + ' '
-        + randomItem(noms)
-        + (coinFlip() === 0 ?
-            ' qui '
-            + randomItem(phraseFin)
-            : ' ' + randomItem(adjectifs))
-        + '.'
-        + (coinFlip() === 0 ? ' ' + randomItem(interjectionFin) : '');
+  const date = new Date().toISOString().slice(0, 10);
+  const tweetContent =
+      (coinFlip() === 0 ? randomItem(interjectionDébut) + ' ' : '')
+      + randomItem(phraseDébut)
+      + ' '
+      + randomItem(noms)
+      + (coinFlip() === 0 ?
+      ' qui '
+      + randomItem(phraseFin)
+      : ' ' + randomItem(adjectifs))
+      + '.'
+      + (coinFlip() === 0 ? ' ' + randomItem(interjectionFin) : '');
 
-    T.post('statuses/update', { status: tweetContent }, (err, data, resp) => {
-        if (err) {
-            console.log('error: ', err);
-        } else {
-            console.log('response: ', resp);
-}
-});
+  T.get('search/tweets', { q: `list:facho_bot/merdias since:${date}`, count: 10 }, (getErr, getData) => {
+    const randomTweet = randomItem(getData.statuses);
+    const tweetId = randomTweet.id_str;
+    const username = randomTweet.user.screen_name;
+
+    T.post('statuses/update', { status: `@${username} ${tweetContent}`, in_reply_to_status_id: tweetId });
+  });
 };
 
-tweet();
 
+tweet();
 setInterval(() => {
-    try {
-        tweet();
-} catch (e) {
+  try {
+    tweet();
+  } catch (e) {
     console.log(e);
-}
-}, 1000 * 60 * 60 * 3); // tweets every 3 hours
+  }
+}, 1000 * 60 * 60 * 2); // tweets every 2 hours
